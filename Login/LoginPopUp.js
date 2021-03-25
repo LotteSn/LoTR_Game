43291815978f5360d23b703187b88e8033339d88
David De Vreese <david.de.vreese@hotmail.be>
2021-03-25 09:56:09 +0100

    prev code

--- Login/LoginPopUp.js
+++ Login/LoginPopUp.js
@@ -3,9 +3,19 @@ let Login = Boolean;
 
 
 function myFunction() {
+    if (Login == false){
         window.location.href = '/login/LoginPagina.html';
+        let Popup = "Login";
+        console.log(Popup);
+        alert(Popup);
 
     }
+    else{
+        alert("ok");
+        //window.location.href = '/login/LoginPagina.html';
+    }
+    
+  }
   function LoginG() {
     if (Login == false){
         
@@ -13,6 +23,9 @@ function myFunction() {
     }
     else{
         window.location.href = '/login/LoginPagina.html';
+        let Popup = "Login";
+        console.log(Popup);
+        alert(Popup);
     }
     
   }
