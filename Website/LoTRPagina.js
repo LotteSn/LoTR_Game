9a25942415e947da0a5cbde2d6bbf7d727c91eef
David De Vreese <david.de.vreese@hotmail.be>
2021-05-21 19:28:58 +0200

    Help en Notepad toegevoegd. Help gaat naar de fandom wiki van LoTR.

--- Website/LoTRPagina.js
+++ Website/LoTRPagina.js
@@ -1,6 +1,6 @@
-src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js";
-src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.bundle.min.js";
-src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js";
+src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.bundle.min.js";
+src = "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js";
 const fetch = require('node-fetch');
 
 // togglePopup
@@ -14,10 +14,11 @@ var token = "L-r_2iO5N7DkmeeHq1V6";
 var uri = 'https://the-one-api.dev/v2/quote';
 async function callAPI() {
     let response = await fetch(uri, {
-    headers: {
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${token}`
-    }})
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
     let data = await response.json();
     return data;
 }
@@ -42,3 +43,33 @@ callAPI().then(data => console.log(data));
 // startButton.onclick = function () { getQuote() };
 
 // var quote = document.getElementById("myQuote");
+
+// Notepad show / hide
+var a;
+function show_hide_notepad() {
+    if (a == 1) {
+        document.getElementById("notepad").style.display = "inline";
+        return a = 0;
+    }
+    else {
+        document.getElementById("notepad").style.display = "none";
+        return a = 1;
+    }
+}
+
+//Notepad Opslagen
+function saveNote() {
+    var textElement = document.getElementById("noteText").value;
+    localStorage.setItem("note", (textElement));
+    console.log(localStorage.getItem("note"));
+}
+
+function loadTextarea() {
+    document.getElementById("noteText").value = localStorage.getItem("note");
+}
+
+//Load Functions voor notepads
+function loadFunctions() {
+    show_hide_notepad();
+    loadTextarea();
+}
