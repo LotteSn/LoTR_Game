3b3725e6b62f4b5f397c3608bb4ecd66c1c452dd
Lotte Snijders <lotte.sn2@gmail.com>
2021-05-23 18:42:20 +0200

    get random quote

--- Website/LoTRPagina.js
+++ Website/LoTRPagina.js
@@ -1,7 +1,17 @@
 src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js";
 src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.bundle.min.js";
 src = "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js";
+src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"
+src = "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
 const fetch = require('node-fetch');
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+var token = "L-r_2iO5N7DkmeeHq1V6";
+var uriQuotes = 'https://the-one-api.dev/v2/quote';
 
 // togglePopup
 function togglePopup() {
@@ -10,39 +20,41 @@ function togglePopup() {
 }
 
 // call API
-var token = "L-r_2iO5N7DkmeeHq1V6";
-var uri = 'https://the-one-api.dev/v2/quote';
-async function callAPI() {
-    let response = await fetch(uri, {
+// async function callAPI() {
+//     let response = await fetch(uriQuotes, {
+//         headers: {
+//             'Accept': 'application/json',
+//             'Authorization': `Bearer ${token}`
+//         }
+//     })
+//     let data = await response.json();
+//     return data;
+// }
+// callAPI();
+// .then(data => console.log(data));
+
+// get quote
+async function fillData() {
+    // GET ALL QUOTES
+    let allQuotesResponse = await fetch(uriQuotes, {
         headers: {
             'Accept': 'application/json',
             'Authorization': `Bearer ${token}`
         }
     })
-    let data = await response.json();
-    return data;
+    let allQuotes = await allQuotesResponse.json();
+    var randomInt = getRandomInt(allQuotes.docs.length);
+    var randomQuote = allQuotes.docs[randomInt].dialog;
+    console.log(randomQuote);
+
+    // var characterBox = document.getElementById("characterName");
+    // var movieBox = document.getElementById("movieName");
+
+    // Y U NO WORK
+    // document.getElementById("myQuote").innerHTML = randomQuote;
 }
-callAPI().then(data => console.log(data));
 
-// // get quote
-// function getQuote() {
-//     var randomId = Math.floor(Math.random() * 2390); 
-
-//     get(`${uri}/${randomId}`)
-//         .done(function (data) {
-//             // quote.innerHTML = data.dialog;
-//             console.log(data.dialog);
-//         })
-//         .fail(function (oError) {
-//             console.log(oError);
-//         });
-// }
-
-// getQuote();
-// var startButton = document.getElementById("startButtonContainer");
-// startButton.onclick = function () { getQuote() };
-
-// var quote = document.getElementById("myQuote");
+fillData();
 
 // Notepad show / hide
 var notepadStatus;
